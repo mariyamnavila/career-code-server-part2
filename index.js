1a5e6eec3fb48408178f47aae8fa5d7af37c74b9
mariyamnavila <bibimariyamnavila@gmail.com>
2025-11-14 19:12:05 +0600

    added firebase base64

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-admin-service-key.json");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
@@ -164,8 +166,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
